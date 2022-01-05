d7bbb1f8a0d5ae7c59cadc46116e0a271bf9985e
gabrielgodoy72 <gabriel.godoy@fiuni.edu.py>
2022-01-05 17:02:17 -0300

    cambio del orden de mascotas en el home

--- frontend/components/cards.jsx
+++ frontend/components/cards.jsx
@@ -4,7 +4,7 @@ const Cards = ({archivos, imagenes}) => {
     return (
         <div className="d-flex flex-wrap px-2">
             { Array.isArray(archivos) && Array.isArray(imagenes) &&
-                archivos.map( archivo => 
+                archivos.reverse().map( archivo => 
                     (<Card key={archivo.Id} archivo={archivo} imagen={imagenes.filter(img => img.FichaId === archivo.Id)}/>)) 
             }
         </div>

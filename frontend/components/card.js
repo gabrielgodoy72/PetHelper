d1dc9c018c1392c49d8a1a4e6474916ba2fc4969
gabrielgodoy72 <gabriel.godoy@fiuni.edu.py>
2022-01-04 23:09:12 -0300

    Agregado de distintivo entre reportados y para adoptar

--- frontend/components/card.js
+++ frontend/components/card.js
@@ -1,9 +1,9 @@
 const Card = ({archivo, imagen}) => {
-    {console.log(archivo.Id, imagen)}
+    {console.log(archivo.Estado.Nombre)}
     return (
         <div className="card" style={{width: '10rem'}}>
             <img src={`/${imagen[0].Nombre}.png`} className="card-img-top" alt={`/${imagen[0].Nombre}.png`} />
-            <div className="card-body">
+            <div className={`card-body ${(archivo.Estado.Nombre === 'Para Adoptar')? 'bg-two' : 'bg-three'}`}>
                 <p className="card-text">{archivo.Raza.Especie.Nombre}</p>
             </div>
         </div>

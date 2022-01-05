351a1f92eecdd16739fc608339c51f7dc89a0bfd
gabrielgodoy72 <gabriel.godoy@fiuni.edu.py>
2022-01-04 23:18:55 -0300

    Estilizado de cards

--- frontend/components/card.js
+++ frontend/components/card.js
@@ -1,10 +1,10 @@
 const Card = ({archivo, imagen}) => {
     {console.log(archivo.Estado.Nombre)}
     return (
-        <div className="card" style={{width: '10rem'}}>
+        <div className="card flex-grow-1 mx-2 my-2 border border-dark" style={{width: '8rem'}}>
             <img src={`/${imagen[0].Nombre}.png`} className="card-img-top" alt={`/${imagen[0].Nombre}.png`} />
             <div className={`card-body ${(archivo.Estado.Nombre === 'Para Adoptar')? 'bg-two' : 'bg-three'}`}>
-                <p className="card-text">{archivo.Raza.Especie.Nombre}</p>
+                <p className="card-text text-white">{archivo.Raza.Especie.Nombre}</p>
             </div>
         </div>
     )
--- frontend/components/cards.js
+++ frontend/components/cards.js
@@ -2,7 +2,7 @@ import Card from "./card";
 
 const Cards = ({archivos, imagenes}) => {
     return (
-        <div className="d-flex flex-wrap justify-content-around bg-one">
+        <div className="d-flex flex-wrap bg-one px-2">
             { Array.isArray(archivos) && Array.isArray(imagenes) &&
                 archivos.map( archivo => 
                     (<Card key={archivo.Id} archivo={archivo} imagen={imagenes.filter(img => img.FichaId === archivo.Id)}/>)) 

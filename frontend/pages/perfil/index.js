cfd48fc9ee9dacbdfd89dc3887c48b7ced6f2590
Andrea Servin <andrea.servin@fiuni.edu.py>
2022-01-13 18:43:23 -0300

    Agrego usuario logueado a pag Mi Perfil
    Aun no esta terminado

--- frontend/pages/perfil/index.js
+++ frontend/pages/perfil/index.js
@@ -1,8 +1,11 @@
 import Celular from "../../components/celular"
 import Head from "next/head"
 import Menu from "../../components/menu"
+import useApplicationContext from "../../hooks/useApplicationContext"
 
 const Perfil = () => {
+
+    const { user } = useApplicationContext()
 
     return (
         <div>
@@ -12,8 +15,13 @@ const Perfil = () => {
             <Celular> 
                 <div className="font-montserrat">
                     <Menu title={'Mi Perfil'}>
-                        <h1 className="text-white">Mi Perfil</h1>
-                        <h6 className="text-white">Pagina que muestra el perfil del usuario que inició sesión</h6>
+                        <h1>Mi Perfil</h1>
+                        <h6>Pagina que muestra el perfil del usuario que inició sesión</h6>
+                        <p>
+                            {
+                                user !== null ? user.Usuario.Id + " - " + user.Usuario.Email : "Loading user..."
+                            }
+                        </p>
                     </Menu>
                 </div>
             </Celular>
